bd17daf7006c62cb8ddd4d61991dd4a6d854a6ca
Matthew White <matt.white@readytech.io>
2021-01-28 11:59:30 +1100

    First rspec example. Had to add another gem to the project (test-unit) in order to run the test. Not sure how schema version bump occurred.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Student, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "expects the factory to be valid" do
+    expect(FactoryBot.build(:student)).to be_valid
+  end
 end
