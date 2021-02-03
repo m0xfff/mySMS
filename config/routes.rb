9fbc94704881675741622ca799f0735b7af74010
Matthew White <matt.white@readytech.io>
2021-02-03 15:49:43 +1100

    Restrict the students resource to just the index method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 MySMS::Application.routes.draw do
-  resources :students
+  resources :students, only: [:index]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
