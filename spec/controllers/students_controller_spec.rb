3d340bbd8b4abdc2e5264f4d9916417ee68a49ec
Matthew White <matt.white@readytech.io>
2021-02-03 16:47:50 +1100

    StudentsController spec tests that it renders the correct template

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe StudentsController, :type => :controller do
-
   describe "GET #index" do
+    subject { get :index }
+
     it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      expect(subject).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      expect(subject).to render_template("students/index")
+    end
+
+    context "with render_views" do
+      render_views
+
+      it "has a Students heading" do
+        expect(subject.body).to match /<h1>Students</
+      end
     end
   end
-
 end
