bc8908a094a6b539625c0a9f38d88263884e7d56
Matthew White <matt.white@readytech.io>
2021-02-05 09:02:20 +1100

    adds gender field to views & replaces date representation to strftime

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
   attr_accessible :birth_date, :email, :first_name, :gender, :last_name, :middle_name, :title
 
-  DEFAULT_PER_PAGE = 5
+  DEFAULT_PER_PAGE = 20
 
   def full_name
     [title, first_name, middle_name, last_name].select(&:present?).join(' ')
