d5f69c9bebb3fba4348259425a7b3632471909e2
Matthew White <matt.white@readytech.io>
2021-02-12 15:30:20 +1100

    adds validations to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,18 @@ class Student < ActiveRecord::Base
   attr_accessible :birth_date, :email, :first_name, :gender, :last_name, :middle_name, :title
 
   DEFAULT_PER_PAGE = 20
+
+  before_save :downcase_email
+
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+
+  private
+
+  def downcase_email
+    self.email.downcase
+  end
 end
