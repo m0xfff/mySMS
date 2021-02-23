a190c89c36da4a5ab802c8c2b047449cb897f127
Matthew White <matt.white@readytech.io>
2021-02-23 15:08:07 +1100

    adds before_filter to application_controller that checks the locale using http_accept_language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+  private
+
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    available = %w(en en-NZ en-AU)
+    I18n.locale = http_accept_language.preferred_language_from(available) || I18n.default_locale
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
 end
