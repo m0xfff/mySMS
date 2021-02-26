10005a2b3d6e652df1b4605ea23174da337c346b
Matthew White <matt.white@readytech.io>
2021-02-26 11:02:06 +1100

    adds numeric and date validations to course, course spec reflects that

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Course, :type => :model do
-  subject { build(:course, name: "Criminology", description: "A Course", number_of_semesters: 2,
-                  start_date: "2021-02-12", end_date: "2021-10-29", allocation: 30) }
+  subject { build(:course) }
 
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:allocation) }
+  it { is_expected.to validate_presence_of(:start_date) }
+  it { is_expected.to validate_presence_of(:end_date) }
+
+  it { is_expected.to validate_numericality_of(:allocation).is_greater_than(0) }
+  it { is_expected.to validate_numericality_of(:number_of_semesters).is_greater_than(0) }
+
   it { is_expected.to validate_length_of(:name).is_at_most(255) }
 
-  it { is_expected.to validate_presence_of(:allocation) }
-  it { is_expected.to validate_numericality_of(:allocation) }
+  it "is invalid if end_date is before start_date" do
+    course = build(:course, start_date: "2021-01-1", end_date: "2020-01-01")
+    course.valid?
+    expect(course.errors[:end_date]).to include("must be after start date")
+  end
 end
