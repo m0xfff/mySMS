85e58935591a2e0a19934059050376a25381d1c7
Matthew White <matt.white@readytech.io>
2021-03-01 15:00:03 +1100

    adds institute tests + removed reference to courses in schema

---
+++ spec/models/institute_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Institute, :type => :model do
+  subject { build(:institute) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_most(255) }
+  it { is_expected.to validate_uniqueness_of(:name) }
+end
